712762aed5cd8df6d6fa7007df8ef4b927af344d
leonardo <leobgsn@gmail.com>
2022-10-24 18:15:28 -0300

    Create bot de automatização no pc.py

---
+++ bot de automatização no pc.py
@@ -0,0 +1,21 @@
+import pyautogui 
+
+pyautogui.PAUSE = 5
+
+
+pyautogui.press("win")
+pyautogui.write("Chrome")
+pyautogui.press("enter")
+pyautogui.write("facebook")
+pyautogui.press("enter")
+
+
+
+
+
+
+#caso você queira pricise pegar a posição do seu mouse na tela, deixo a baixo o código.
+#import time
+
+#time.sleep(3)
+#pyautogui.position()
